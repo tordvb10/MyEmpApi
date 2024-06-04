2ce62823c9549ef2e4a359ad9d321eeff57264b1
tordvb10 <tordvb10.kodehode@gmail.com>
2024-06-04 13:44:10 +0200

    DatabaseHelper

---
+++ MyEmpApi/Data/DataHelper.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MyEmpApi.Data
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+        public DatabaseHelper (string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public DataTable ExecuteQuery(stirng query)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
